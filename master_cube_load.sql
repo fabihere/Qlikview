52ddebebc6edf56e924d0e8a1adb1b96ef0f1eb6
fabihere <fabianayu@gmail.com>
2015-12-31 04:05:44 -0200

    updated latest versions of master loads

--- master_cube_load.sql
+++ master_cube_load.sql
@@ -13,8 +13,10 @@ SET DayNames='Mon;Tue;Wed;Thu;Fri;Sat;Sun';
 
 //Creates the main data cubes
 
-
+//Pull raw data qvds from
 set vpathQVD='D:\QlikView\DataSource\';
+
+//Save master data cubes to
 set vpathCube='D:\QlikView\DataCube\';
 
 
@@ -22,7 +24,9 @@ set vpathCube='D:\QlikView\DataCube\';
 
 
 
-///$tab Maps
+///$tab UTM Groups
+
+//Finds all distinct utm sources to create a To-From table of all distinct source data into key utm groups
 distinct_source:
 LOAD
 	distinct(source) as source
@@ -34,6 +38,7 @@ LOAD
 FROM [$(vpathQVD)utms.qvd] (qvd)
 WHERE isnull(source);
 
+//Creates utm groups based on marketing: as of 2015-12, grouped emkt, push, direct, unattributed and groups all others (other paid channels)
 group_source:
 LOAD
 	source,
@@ -72,7 +77,7 @@ WHERE NOT wildmatch(source,'*emkt*','*welcome*','*howit*','*baseat*','*push*','*
 
 drop table distinct_source;
 
-
+//Creates full utm cube with utm source group defined above and exports cube
 utms:
 LOAD id as utm_id,
     if(isnull(source),'null',source) as source,
@@ -88,6 +93,10 @@ Resident group_source;
 
 drop table group_source;
 
+store utms into [$(vpathCube)utmscube.qvd] (qvd);
+
+///$tab Maps
+//Creates maps
 utm_group:
 mapping
 LOAD utm_id,
@@ -119,7 +128,7 @@ LOAD * INLINE [
     origin, device
     1, Website
     2, iOS app
-    3, Third Party
+    3, Website
     4, Android app
     5, Mobile browser
 ];
@@ -139,143 +148,496 @@ LOAD state,
 FROM
 D:\QlikView\Dados\Dropbox\Qlikview\orderstate.xlsx
 (ooxml, embedded labels, table is Sheet1);
-
-
-
 ///$tab Users Cube
-//users:
-//load  id as user_id,
-//    email,
-//    first_name,
-//    last_name,
-//    gender,
-//    applymap('email_subscription',subscribed,'null') as email_subscription,
-//	applymap('utm_source',utm_id,'no utm') as user_source,
-//		applymap('utm_medium',utm_id,'no utm') as user_medium,
-//			applymap('utm_campaign',utm_id,'no utm') as user_campaign,
-//   				 applymap('origin',origin,'no utm') as user_origin,
-//   				 	applymap('utm_group',utm_id,'no utm') as user_source_group,
-//    				date(	floor(ConvertToLocalTime(created_at,'Brasilia',false())),'MM/DD/YYYY') as register_date
-//FROM [$(vpathQVD)users.qvd] (qvd);
-//
-//LEFT JOIN
-//load id as user_id,
-//	1 as user_app_count
-//FROM [$(vpathQVD)users.qvd] (qvd)
-//WHERE Match(origin,'2','4');
-//
-//LEFT JOIN
-//load id as user_id,
-//	1 as user_desktopbrowswer_count
-//FROM [$(vpathQVD)users.qvd] (qvd)
-//WHERE Match(origin,'1','3','5');
-//
-//store users into [$(vpathCube)userscube.qvd] (qvd);
+//Creates and exports users cube
+users:
+load  id as user_id,
+    email,
+    first_name,
+    last_name,
+    gender,
+    subscribed as is_opt_in,
+
+    applymap('email_subscription',subscribed,'null') as email_subscription,
+	applymap('utm_source',utm_id,'no utm') as user_source,
+		applymap('utm_medium',utm_id,'no utm') as user_medium,
+			applymap('utm_campaign',utm_id,'no utm') as user_campaign,
+   				 applymap('origin',origin,'no utm') as user_origin,
+   				 	applymap('utm_group',utm_id,'no utm') as user_source_group,
+    				date(	floor(ConvertToLocalTime(created_at,'Brasilia',false())),'MM/DD/YYYY') as register_date,
+    				num(year(ConvertToLocalTime(created_at,'Brasilia',false()))) as register_year,
+							num(year(ConvertToLocalTime(created_at,'Brasilia',false())))&'-'&num(month(ConvertToLocalTime(created_at,'Brasilia',false())),'00') as register_cohort,
+								date(	floor(ConvertToLocalTime(most_recent_access,'Brasilia',false())),'MM/DD/YYYY') as last_web_access_date,
+									num(year(ConvertToLocalTime(most_recent_access,'Brasilia',false())))&'-'&num(month(ConvertToLocalTime(most_recent_access,'Brasilia',false())),'00') as web_access_cohort
+FROM [$(vpathQVD)users.qvd] (qvd);
+
+LEFT JOIN
+load
+	id as user_id,
+		  if ( origin = 2, 1, if ( origin = 4, 1, 0)) as registered_via_app,
+			if ( origin = 4 , 1, 0) as registered_via_mobile_browser,
+				if(origin = 1, 1, if (origin = 3, 1,0)) as registered_via_desktop,
+					if(IsNull(most_recent_access),0,1) as accesed_via_web
+FROM [$(vpathQVD)users.qvd] (qvd);
+
+LEFT JOIN
+load
+	user_id,
+	1 as accessed_via_app,
+	date(	floor(ConvertToLocalTime(most_recent_access,'Brasilia',false())),'MM/DD/YYYY') as last_app_access_date,
+									num(year(ConvertToLocalTime(most_recent_access,'Brasilia',false())))&'-'&num(month(ConvertToLocalTime(most_recent_access,'Brasilia',false())),'00') as app_access_cohort
+FROM [$(vpathQVD)app_sessions.qvd] (qvd);
+
+store users into [$(vpathCube)userscube.qvd] (qvd);
 ///$tab Orders Cube
+//Creates new column total_stockout_quantity to include orders with TOTAL STOCKOUT into calculation
+temp:
+LOAD
+	id as order_id
+FROM [$(vpathQVD)orders.qvd] (qvd)
+WHERE match(state, 'cancelled_stockout');
+
+LEFT JOIN
+LOAD
+	order_id,
+	order_id&'|'&id as order_item_key,
+	quantity as stockout_quantity
+FROM [$(vpathQVD)order_items.qvd] (qvd);
+
+cancelled_stockout:
+mapping
+LOAD
+	order_item_key,
+	stockout_quantity
+Resident temp;
+
+drop table temp;
+
+//Creates new column total_cancelled_quantity into calculation
+
+temp1:
+LOAD
+	id as order_id
+FROM [$(vpathQVD)orders.qvd] (qvd)
+WHERE match(state, 'cancelled');
+
+LEFT JOIN
+LOAD
+	order_id,
+	order_id&'|'&id as order_item_key,
+	quantity as cancelled_quantity
+FROM [$(vpathQVD)order_items.qvd] (qvd);
+
+cancelled:
+mapping
+LOAD
+	order_item_key,
+	cancelled_quantity
+Resident temp1;
+
+drop table temp1;
+
+//Creates and exports order items cube with most recent order data
+
 order_items:
 LOAD
    	order_id,
+   	id,
+   	campaign_id,
+   	variant_id as productkey,
    	quantity,
     final_price,
+    original_price,
     cancelled_quantity,
     stockout_quantity,
     shipping_price,
-    (quantity*final_price)+shipping_price+stockout_shipping_price as original_item_value,
-			((quantity-cancelled_quantity-stockout_quantity)*final_price)+shipping_price as final_item_value,
-			quantity-cancelled_quantity-stockout_quantity as final_item_quantity,
-				cancelled_quantity+stockout_quantity as cancelledstockout_quantity
+    stockout_shipping_price,
+	order_id&'|'&id as order_item_key
 FROM [$(vpathQVD)order_items.qvd] (qvd);
+
+order_items1:
+LOAD
+   	*,
+	ApplyMap('cancelled_stockout',order_item_key,stockout_quantity) as all_stockout_quantity,
+		ApplyMap('cancelled',order_item_key,cancelled_quantity) as all_cancelled_quantity
+Resident order_items;
+
+order_items2:
+LOAD
+	*,
+	    (quantity*final_price)+shipping_price+stockout_shipping_price as original_item_value,
+			((quantity-all_cancelled_quantity-all_stockout_quantity)*final_price)+shipping_price as final_item_value,
+			quantity-all_cancelled_quantity-all_stockout_quantity as final_item_quantity,
+				all_stockout_quantity+all_cancelled_quantity as unsold_quantity
+Resident order_items1;
+
+store order_items2 into [$(vpathCube)orderitemscube.qvd] (qvd);
+
+drop table order_items;
+drop table order_items1;
+
+
+//Calculates most recent data by order
 
 orders1:
 LOAD
 	order_id,
+	count(id) as number_of_products,
 	sum(original_item_value) as original_order_value,
 	sum(quantity) as original_quantity,
-	sum(cancelled_quantity) as cancelled_quantity,
-	sum(stockout_quantity) as stockout_quantity,
-	sum(cancelledstockout_quantity) as cancelledstockout_quantity,
+	sum(all_cancelled_quantity) as final_cancelled_quantity,
+	sum(all_stockout_quantity) as final_stockout_quantity,
+	sum(unsold_quantity) as final_unsold_quantity,
 	sum(final_item_quantity) as final_quantity,
 	sum(final_item_value) as final_order_value
-Resident order_items
+Resident order_items2
 Group by order_id;
 
-orders:
+//Creates and exports authorized orders cube:
+temp3:
 Load
-	id as order_id
-FROM [$(vpathQVD)orders.qvd] (qvd)
-WHERE match(state, 'captured', 'invoiced','delivered','shipping_successful');
+	id as order_id,
+	state,
+	ApplyMap('consider',state,'error')as consider
+FROM [$(vpathQVD)orders.qvd] (qvd);
+
+authorized_orders:
+LOAD
+	order_id,
+	state
+Resident temp3
+WHERE consider = 'yes';
 
 LEFT JOIN
 Load id as order_id,
     user_id,
     user_id&'|'&id as ftpkey,
 	date(	floor(ConvertToLocalTime(created_at,'Brasilia',false())),'MM/DD/YYYY') as order_date,
-    state,
+	num(year(ConvertToLocalTime(created_at,'Brasilia',false())))&'-'&num(month(ConvertToLocalTime(created_at,'Brasilia',false())),'00') as order_cohort,
+		num(year(ConvertToLocalTime(created_at,'Brasilia',false()))) as order_year,
   	applymap('utm_source',utm_id,'no utm') as order_source,
 		applymap('utm_medium',utm_id,'no utm') as order_medium,
 			applymap('utm_campaign',utm_id,'no utm') as order_campaign,
    				 applymap('origin',origin,'no utm') as order_origin,
-   				 	applymap('utm_group',utm_id,'no utm') as order_source_group
+   				 	applymap('utm_group',utm_id,'no utm') as order_source_group,
+   				 	  if ( origin = 2, 1, if ( origin = 4, 1, 0)) as via_app,
+							if ( origin = 4 , 1, 0) as via_mobile_browser,
+									if(origin = 1, 1, if (origin = 3, 1,0)) as via_desktop,
+										state,
+										user_id&'|'&num(year(ConvertToLocalTime(created_at,'Brasilia',false())))&'-'&num(month(ConvertToLocalTime(created_at,'Brasilia',false())),'00') as user_order_key
 FROM [$(vpathQVD)orders.qvd] (qvd);
 
 LEFT JOIN
 Load order_id,
 	original_order_value,
 	original_quantity,
-	cancelled_quantity,
-	stockout_quantity,
-	cancelledstockout_quantity,
+	final_cancelled_quantity,
+	final_stockout_quantity,
+	final_unsold_quantity,
 	final_quantity,
 	final_order_value,
-    1 as order_count
+    1 as is_considered_order
 Resident orders1;
 
-temp:
-LOAD
-	order_id,
-	if(isnull(Previous(user_id)),1,
-		if(user_id=Previous(user_id) and order_date > previous(order_date), peek('RowNumber')+1,
-			if(user_id <> Previous(user_id) and order_date > previous(order_date), 1))) as Rownumber,
-			AutoNumber(order_id,user_id) as test
-Resident orders;
+LEFT JOIN
+Load order_id,
+	1 as is_billed_order
+Resident temp3
+Where match(state,'shipping_successful','delivered');
 
-Store orders into [$(vpathCube)orderscube.qvd] (qvd);
+drop table temp3;
 
-drop table order_items;
+store authorized_orders into [$(vpathCube)authorizedorderscube.qvd] (qvd);
+
 drop table orders1;
 
 
 
 
 
+//temp4:
+//LOAD
+//	order_id,
+//	if(isnull(Previous(user_id)),1,
+//		if(user_id=Previous(user_id) and order_date > previous(order_date), peek('RowNumber')+1,
+//			if(user_id <> Previous(user_id) and order_date > previous(order_date), 1))) as Rownumber,
+//			AutoNumber(order_id,user_id) as test
+//Resident authorized_orders;
+//
+//Store orders into [$(vpathCube)orderscube.qvd] (qvd);
+//
+//drop table order_items;
+//drop table orders1;
+//
+//
+//
+//
+
 
 
 ///$tab FTB Cube
-//ftbs:
-//load user_id,
-//	min(order_id) as ftp,
-//	date(min(order_date)) as ftp_order_date,
-//	MonthName(date(min(order_date))) as ftp_cohort,
-//	user_id&'|'&min(order_id) as ftpkey
-//Resident orders
-//WHERE match(state, 'invoiced','delivered','shipping_successful')
-//Group by user_id;
-//
-//STORE ftbs into [$(vpathCube)ftbcube.qvd] (qvd);
-//
-//LEFT JOIN (users)
-//LOAD
-//	user_id,
-//	ftp,
-//	ftp_order_date,
-//	ftp_cohort
-//Resident ftbs;
-//
-//LEFT JOIN (orders)
-//LOAD
-//	ftpkey,
-//	1 as ftb_count
-//Resident ftbs;
-//
+//Creates FTB chart, excluding unauthorized or cancelled orders
+
+ftbs:
+load user_id,
+	min(order_id) as ftp_order_id,
+	date(min(order_date)) as ftp_order_date,
+	num(year(min(order_date)))&'-'&num(month(min(order_date)),'00') as ftb_cohort,
+	user_id&'|'&min(order_id) as ftpkey,
+	1 as ftb_count
+Resident authorized_orders
+Group by user_id;
+
+STORE ftbs into [$(vpathCube)ftbcube.qvd] (qvd);
+
+temp5:
+LOAD
+	user_id
+Resident users;
+
+LEFT JOIN
+LOAD
+	user_id,
+	1 as ftb_count,
+	ftb_cohort
+Resident ftbs;
+
+temp6:
+LOAD
+	user_id,
+	ftb_count as is_buyer,
+	ftb_cohort
+Resident temp5
+where ftb_count = 1;
+
+Concatenate
+LOAD
+	user_id,
+	0 as is_buyer,
+	0 as ftb_cohort
+Resident temp5
+where ftb_count <> 1;
+
+drop table temp5;
+
+LEFT JOIN (users)
+load
+	user_id,
+	is_buyer,
+	ftb_cohort
+Resident temp6;
+
+drop table temp6;
+
+LEFT JOIN (authorized_orders)
+LOAD
+	ftp_order_id as order_id,
+	1 as is_ftb
+Resident ftbs;
+
+drop fields user_id, ftb_cohort from ftbs;
+
 //drop table ftbs;
-///$tab Tests
+
+
+///$tab Buyers
+temp7:
+LOAD
+	order_cohort,
+	user_id,
+	count(DISTINCT(order_id))
+Resident authorized_orders
+Group by order_cohort,user_id;
+
+left join
+load
+	user_id,
+	ftb_cohort
+Resident users;
+
+buyers:
+LOAD
+	user_id&'|'&order_cohort as user_order_key,
+	if(order_cohort=ftb_cohort,1,0) as is_ftb_in_month,
+	if(order_cohort=ftb_cohort,'FTBs','Repeat Buyers') as buyer_type
+Resident temp7;
+
+drop table temp7;
+
+
+
+///$tab Product Dimensions Cube
+tags:
+LOAD
+	id as    age_id,
+	id as    size_id,
+	id as    category_id,
+	id as     vertical_id,
+	id as gender_id,
+	id as subcategory_id,
+    type,
+    label
+FROM [$(vpathQVD)tags.qvd] (qvd);
+
+agetag:
+mapping
+load
+	age_id,
+	label
+Resident tags;
+
+sizetag:
+mapping
+load
+	size_id,
+	label
+Resident tags;
+
+categorytag:
+mapping
+load
+	category_id,
+	label
+Resident tags;
+
+verticaltag:
+mapping
+load
+	vertical_id,
+	label
+Resident tags;
+
+gendertag:
+mapping
+load
+	gender_id,
+	label
+Resident tags;
+
+subcategorytag:
+mapping
+load
+	subcategory_id,
+	label
+Resident tags;
+
+drop table tags;
+
+brands:
+LOAD id as brand_id,
+    name,
+    slug
+FROM [$(vpathQVD)brands.qvd] (qvd);
+
+brandtag:
+Mapping
+LOAD brand_id,
+    name as brand_name
+Resident brands;
+
+drop table brands;
+
+products_dimension:
+//load variant-level dimensions first then left join with sku-level dimensions
+LOAD
+    id as productkey,
+    parent_product_id,
+   parent_product_id as skukey,
+    applymap('agetag',age_id,'none') as [Age Group],
+	applymap('sizetag',    size_id,	'none') as Size,
+	supplier_size as [Supplier Size],
+	date(Floor(ConvertToLocalTime(created_at,'Brasilia',false())),'MM/DD/YYYY') as [Product Date],
+	num(month(ConvertToLocalTime(created_at,'Brasilia',false())),'00')&'-'&num(year(ConvertToLocalTime(created_at,'Brasilia',false()))) as [Product Year-Month],
+	date(Floor(ConvertToLocalTime(updated_at,'Brasilia',false())),'MM/DD/YYYY') as [Product Updated],
+
+	// Adicionado por David Bernardi - 08/12/15
+	name,
+	original_price as product_original_price,
+	final_price	   as product_final_price,
+	erp_code,
+	supplier_color
+FROM [$(vpathQVD)products.qvd] (qvd)
+WHERE NOT isnull(parent_product_id);
+
+LEFT JOIN
+LOAD
+	id as skukey,
+	brand_id,
+	applymap('brandtag',brand_id,'none') as brand_name,
+	applymap('categorytag',    category_id,'none') as Category,
+	applymap('verticaltag',    vertical_id, 'none') as Segment,
+	ApplyMap('gendertag',    gender_id,'none') as Gender,
+	ApplyMap('subcategorytag',    subcategory_id,'none') as Subcategory
+FROM [$(vpathQVD)products.qvd] (qvd);
+
+CONCATENATE
+LOAD
+	id as productkey,
+	id as skukey,
+	applymap('agetag',age_id,'none') as [Age Group],
+	applymap('sizetag',    size_id,	'none') as Size,
+	supplier_size as [Supplier Size],
+	brand_id,
+	applymap('brandtag',brand_id,'none') as brand_name,
+	applymap('categorytag',    category_id,'none') as Category,
+	applymap('verticaltag',    vertical_id, 'none') as Segment,
+	ApplyMap('gendertag',    gender_id,'none') as Gender,
+	ApplyMap('subcategorytag',    subcategory_id,'none') as Subcategory,
+	date(Floor(ConvertToLocalTime(created_at,'Brasilia',false())),'MM/DD/YYYY') as [Product Date],
+	date(Floor(ConvertToLocalTime(updated_at,'Brasilia',false())),'MM/DD/YYYY') as [Product Updated],
+	num(month(ConvertToLocalTime(created_at,'Brasilia',false())),'00')&'-'&num(year(ConvertToLocalTime(created_at,'Brasilia',false()))) as [Product Year-Month],
+	name,
+	original_price as product_original_price,
+	final_price	   as product_final_price,
+	erp_code,
+	supplier_color
+FROM [$(vpathQVD)products.qvd] (qvd)
+WHERE isnull(parent_product_id)
+AND
+NOT exists (skukey, id);
+
+store products_dimension into $(vpathCube)/product_dimensions_cube.qvd (qvd);
+
+delivery:
+Mapping
+LOAD * INLINE [
+    fast_delivery, delivery
+    1, Entrega Rápida
+    2, Entrega Normal
+];
+
+inventory:
+Mapping
+LOAD * INLINE [
+    inventory_type, inventory
+    FATURAMENTO DINDA, Estoque interno
+    VIRTUAL DINDA, Consignação
+];
+
+campaigns:
+LOAD id as campaign_id,
+    name as [Campaign Name],
+    name as [Campaign Brand],
+    slug as campaign_slug,
+    Year(ConvertToLocalTime(offer_starts_at,'Brasilia',false())) as [Campaign Start Year],
+	MonthName(ConvertToLocalTime(offer_starts_at,'Brasilia',false())) as [Campaign Start Year Month],
+	date(Floor(ConvertToLocalTime(offer_starts_at,'Brasilia',false())),'MM/DD') as [Campaign Starts],
+	ConvertToLocalTime(offer_starts_at,'Brasilia',false()) as offer_starts_att,
+    Year(ConvertToLocalTime(offer_ends_at,'Brasilia',false())) as [Campaign End Year],
+	MonthName(ConvertToLocalTime(offer_ends_at,'Brasilia',false())) as [Campaign End Year Month],
+	date(Floor(ConvertToLocalTime(offer_ends_at,'Brasilia',false())),'MM/DD') as [Campaign Ends],
+	ConvertToLocalTime(offer_ends_at,'Brasilia',false()) as offer_ends_att,
+    ApplyMap('inventory',inventory_type,'noinventorytype') as campaign_type,
+	ApplyMap('delivery', fast_delivery, 'nodeliverytype') as delivery_type
+FROM [$(vpathQVD)campaigns.qvd] (qvd);
+
+
+LEFT JOIN
+LOAD
+    campaign_id,
+    product_id as campaign_product_id,
+    stock as initial_stock
+//    campaign_id&'|'&product_id as campaignproductkey
+FROM [$(vpathQVD)campaigns_products.qvd] (qvd);
+
+//ENTREGA RÁPIDA A PARTIR DE JULHO 2014
--- master_load.sql
+++ master_load.sql
@@ -1,3 +1,4 @@
+///$tab Main
 SET ThousandSep=',';
 SET DecimalSep='.';
 SET MoneyThousandSep=',';
@@ -13,6 +14,108 @@ SET DayNames='Mon;Tue;Wed;Thu;Fri;Sat;Sun';
 set vpathQVD='D:\QlikView\DataSource\';
 set vpathBU='E:\Qlikview\Backup\';
 
+
+ODBC CONNECT TO [MySQL Dinda BI] (XUserId is JHZXPRFNfCZSGXRMBTcB, XPassword is UfCbMRFNELacGSFMSBMCDaYb);
+
+
+
+///$tab app_sessions qvd
+app_sessions:
+LOAD `user_id`,
+    `created_at`,
+    `most_recent_access`;
+SQL SELECT `user_id`,
+    `created_at`,
+    `most_recent_access`
+FROM `dinda_prd`.`app_sessions`;
+
+
+store app_sessions into [$(vpathQVD)app_sessions.qvd] (qvd);
+
+drop table app_sessions;
+///$tab campaigns qvd
+campaigns:
+LOAD id,
+    name,
+    description,
+    slug,
+    `short_description`,
+    `offer_starts_at`,
+    `offer_ends_at`,
+    `logo_from_s3`,
+    `highlight_from_s3`,
+    `mini_logo_from_s3`,
+    banner,
+    `brand_id`,
+    `created_at`,
+    `updated_at`,
+    exclusive,
+    kids,
+    `supplier_time`,
+    `warehouse_time`,
+    `stock_out`,
+    `inventory_id`,
+    `inventory_type`,
+    `ends_exclusive_at`,
+    `fast_delivery`,
+    `scheduled_home_highlight`,
+    `home_highlight_description`;
+SQL SELECT id,
+    name,
+    description,
+    slug,
+    `short_description`,
+    `offer_starts_at`,
+    `offer_ends_at`,
+    `logo_from_s3`,
+    `highlight_from_s3`,
+    `mini_logo_from_s3`,
+    banner,
+    `brand_id`,
+    `created_at`,
+    `updated_at`,
+    exclusive,
+    kids,
+    `supplier_time`,
+    `warehouse_time`,
+    `stock_out`,
+    `inventory_id`,
+    `inventory_type`,
+    `ends_exclusive_at`,
+    `fast_delivery`,
+    `scheduled_home_highlight`,
+    `home_highlight_description`
+FROM `dinda_prd`.campaigns;
+
+
+
+
+store campaigns into [$(vpathQVD)campaigns.qvd] (qvd);
+
+drop table campaigns;
+///$tab Description
+
+
+
+
+
+//ALL TABLES ARE UPDATED IN FULL DAILY @ 1AM, SEE NOTE BELOW
+
+
+
+
+
+
+
+//NEED TO TEST AN INCREMENTAL MASTER UPDATE, WE CAME ACROSS A COUPLE OF ERRORS LAST TIME WE TRIED TO IMPLEMENT IT
+// This is the master reload of DB data. For each table, there are 4 steps:
+
+//1. Initial Load: Creates master file of the table. Use only when needed to reload ALL records or data. Otherwise, keep commented for reference only.
+//2. Backup D-1: Create 1 backup copy of the previous load:
+//3. Perform Incremental Load: Load incremental records from db
+//4. Update Master QVD: Concanate incremental load with master qvd by adding new records or replacing updated records
+
+//The following tables are updated daily at 1:00 am:
 ///$tab orders qvd
 orders:
 
@@ -159,63 +262,6 @@ FROM `dinda_prd`.`campaigns_products`;
 Store campaigns_products into [$(vpathQVD)campaigns_products.qvd] (qvd);
 
 drop table campaigns_products;
-///$tab campaigns qvd
-campaigns:
-LOAD id,
-    name,
-    description,
-    slug,
-    `short_description`,
-    `offer_starts_at`,
-    `offer_ends_at`,
-    `logo_from_s3`,
-    `highlight_from_s3`,
-    `mini_logo_from_s3`,
-    banner,
-    `brand_id`,
-    `created_at`,
-    `updated_at`,
-    exclusive,
-    kids,
-    `supplier_time`,
-    `warehouse_time`,
-    `stock_out`,
-    `inventory_id`,
-    `inventory_type`,
-    `ends_exclusive_at`,
-    `fast_delivery`,
-    `scheduled_home_highlight`,
-    `home_highlight_description`;
-SQL SELECT id,
-    name,
-    description,
-    slug,
-    `short_description`,
-    `offer_starts_at`,
-    `offer_ends_at`,
-    `logo_from_s3`,
-    `highlight_from_s3`,
-    `mini_logo_from_s3`,
-    banner,
-    `brand_id`,
-    `created_at`,
-    `updated_at`,
-    exclusive,
-    kids,
-    `supplier_time`,
-    `warehouse_time`,
-    `stock_out`,
-    `inventory_id`,
-    `inventory_type`,
-    `ends_exclusive_at`,
-    `fast_delivery`,
-    `scheduled_home_highlight`,
-    `home_highlight_description`
-FROM `dinda_prd`.campaigns;
-
-store campaigns into [$(vpathQVD)campaigns.qvd] (qvd);
-
-drop table campaigns;
 ///$tab products qvd
 products:
 LOAD id,
